0f941ca745168509beef406ca7a5d677ad24d58e
cuatedani <cuate.danielito@gmail.com>
2026-02-17 16:42:11 -0700

    Busqueda por nombre

--- src/main/java/Service/PokemonService.java
+++ src/main/java/Service/PokemonService.java
@@ -7,6 +7,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import javax.swing.*;
+
 public class PokemonService {
     OkHttpClient client =  new OkHttpClient();
     Gson gson = new Gson();
@@ -33,6 +35,44 @@ public class PokemonService {
 
         } catch (Exception e) {
             System.out.println("Error al consultar la API: " + e.getMessage());
+            JOptionPane.showMessageDialog(
+                    null,
+                    "No se pudo encontrar el pokemon ingresado",
+                    "Pokemon NO Encontrado",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
+            return null;
+        }
+    }
+
+    public PokemonEntity findByName(String nombre) {
+
+        String url = "https://pokeapi.co/api/v2/pokemon/" + nombre;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("Error HTTP: " + response.code());
+            }
+
+            String jsonString = response.body().string();
+
+            PokemonEntity pokemon = gson.fromJson(jsonString, PokemonEntity.class);
+
+            return pokemon;
+
+        } catch (Exception e) {
+            System.out.println("Error al consultar la API: " + e.getMessage());
+            JOptionPane.showMessageDialog(
+                    null,
+                    "No se pudo encontrar el pokemon ingresado",
+                    "Pokemon NO Encontrado",
+                    JOptionPane.INFORMATION_MESSAGE
+            );
             return null;
         }
     }
