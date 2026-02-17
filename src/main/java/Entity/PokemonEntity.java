b0e5192cda6612d7d96c48f3d96a9097c6de02d9
cuatedani <cuate.danielito@gmail.com>
2026-02-17 16:14:44 -0700

    Ajustes de Aislamiento

--- src/main/java/Entity/PokemonEntity.java
+++ src/main/java/Entity/PokemonEntity.java
@@ -3,9 +3,29 @@ package Entity;
 import java.util.Map;
 
 public class PokemonEntity {
-    int id;
-    String name;
-    double height;
-    double weight;
-    SpritesEntity sprites;
+    private int id;
+    private String name;
+    private double height;
+    private double weight;
+    private SpritesEntity sprites;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public SpritesEntity getSprites() {
+        return sprites;
+    }
 }
--- src/main/java/Entity/SpritesEntity.java
+++ src/main/java/Entity/SpritesEntity.java
@@ -1,6 +1,14 @@
 package Entity;
 
 public class SpritesEntity {
-    String back_default;
-    String front_default;
+    private String back_default;
+    private String front_default;
+
+    public String getBack_default() {
+        return back_default;
+    }
+
+    public String getFront_default() {
+        return front_default;
+    }
 }
