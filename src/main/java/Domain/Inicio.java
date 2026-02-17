eadbde04bdf033720f6049f5edf57abf08f838c0
cuatedani <cuate.danielito@gmail.com>
2026-02-17 16:07:20 -0700

    Buscar Pokemon Service

--- src/main/java/Domain/Inicio.java
+++ src/main/java/Domain/Inicio.java
@@ -1,8 +1,12 @@
 package Domain;
 
+import Service.PokemonService;
+
 import javax.swing.*;
 
 public class Inicio {
+    PokemonService pokeSer = new PokemonService();
+
     public static void main(String[] Args){
         int optMenu = -1;
         String[] botones = {"1.- Buscar Entity.Pokemon", "2.- Salir"};
@@ -20,6 +24,10 @@ public class Inicio {
 
         switch (optMenu){
             case 1:
+                int id = Integer.parseInt(JOptionPane.showInputDialog(null, "Pokeapi Java", "Ingresa un id de pokemon", JOptionPane.QUESTION_MESSAGE));
+                pokeSer.findById(id);
+                break;
+            case 2:
                 break;
             default:
                 break;
--- src/main/java/Service/PokemonService.java
+++ src/main/java/Service/PokemonService.java
@@ -1,5 +1,39 @@
 package Service;
 
-public class PokemonService {
 
+import Entity.PokemonEntity;
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class PokemonService {
+    OkHttpClient client =  new OkHttpClient();
+    Gson gson = new Gson();
+
+    public PokemonEntity findById(int id) {
+
+        String url = "https://pokeapi.co/api/v2/pokemon/" + id;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+
+            if (!response.isSuccessful()) {
+                throw new RuntimeException("Error HTTP: " + response.code());
+            }
+
+            String jsonString = response.body().string();
+
+            PokemonEntity pokemon = gson.fromJson(jsonString, PokemonEntity.class);
+
+            return pokemon;
+
+        } catch (Exception e) {
+            System.out.println("Error al consultar la API: " + e.getMessage());
+            return null;
+        }
+    }
 }
