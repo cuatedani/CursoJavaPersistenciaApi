2af5df1f170475b21f1726ab55fc9f1da9336498
cuatedani <cuate.danielito@gmail.com>
2026-02-17 15:17:45 -0700

    Menu Principal

---
+++ src/main/java/Inicio.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Inicio {
+    public static void main(String[] Args){
+        int optMenu = -1;
+        String[] botones = {"1.- Buscar Pokemon", "2.- Salir"};
+
+        //Menu Principal
+        String opcion = (String) JOptionPane.showInputDialog(null, "Pokeapi Java", "Menu Principal", JOptionPane.INFORMATION_MESSAGE, null,
+                botones, botones[0]);
+
+        //Validar Opcion
+        for (int i=0; i<botones.length; i++){
+            if(opcion.equals(botones[i])){
+                optMenu = i;
+            }
+        }
+
+        switch (optMenu){
+            case 1:
+                break;
+            default:
+                break;
+        }
+    }
+}
